fd8bfefe4ab068c08513660d1f2e6f75aa3cb6ee
viren <vincent.renard@gmx.com>
2021-02-03 16:50:05 +0100

    init

--- Timer/ViewController.swift
+++ Timer/ViewController.swift
@@ -8,10 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var nbrs:Int64=0
+    @IBOutlet weak var timeLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(self.incr), userInfo: nil, repeats: true)
+        //Timer.invalidate(<#T##self: Timer##Timer#>)
+    }
+    @objc func incr() {
+        nbrs+=1;
+        timeLabel.text=nbrs
+        
     }
 
 
